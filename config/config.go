b9a66f31f98414e9b34f88e9ed44b94f69957de9
Pravin Nalawade <pravinonline05@gmail.com>
2026-07-06 16:28:41 +0530

    config setup

---
+++ config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AppPort   string
+	DBHost    string
+	DBUser    string
+	DBPass    string
+	DBName    string
+	DBPort    string
+	JWTSecret string
+}
+
+func Load() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+
+	return &Config{
+		AppPort:   getEnv("APP_PORT", "8080"),
+		DBHost:    getEnv("DB_HOST", "localhost"),
+		DBUser:    getEnv("DB_USER", "postgres"),
+		DBPass:    getEnv("DB_PASS", "postgres"),
+		DBName:    getEnv("DB_NAME", "blogdb"),
+		DBPort:    getEnv("DB_PORT", "5432"),
+		JWTSecret: getEnv("JWT_SECRET", "kfdjsfjosifsfsf85648df"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+
+	return fallback
+}
